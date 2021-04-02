f37ceb38393f77723e24abb2208869cf9a2c8473
Domen Kralj <domenkralj.sahist@gmail.com>
2021-04-03 00:23:06 +0200

    DepartureActivity code clean and network and error handling added

--- app/src/main/java/com/VegaSolutions/lpptransit/ui/activities/lpp/DepartureActivity.java
+++ app/src/main/java/com/VegaSolutions/lpptransit/ui/activities/lpp/DepartureActivity.java
@@ -1,35 +1,30 @@
 package com.VegaSolutions.lpptransit.ui.activities.lpp;
 
-import android.content.res.ColorStateList;
-import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.ColorUtils;
-import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.VegaSolutions.lpptransit.R;
+import com.VegaSolutions.lpptransit.TravanaApp;
 import com.VegaSolutions.lpptransit.lppapi.Api;
 import com.VegaSolutions.lpptransit.lppapi.responseobjects.TimetableWrapper;
 import com.VegaSolutions.lpptransit.ui.animations.ElevationAnimation;
-import com.VegaSolutions.lpptransit.ui.errorhandlers.CustomToast;
 import com.VegaSolutions.lpptransit.utility.Colors;
+import com.VegaSolutions.lpptransit.utility.NetworkConnectivityManager;
+import com.VegaSolutions.lpptransit.utility.ScreenState;
 import com.VegaSolutions.lpptransit.utility.ViewGroupUtils;
 import com.google.android.flexbox.FlexboxLayout;
 
@@ -39,10 +34,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.SimpleTimeZone;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import static com.VegaSolutions.lpptransit.utility.ScreenState.DONE;
+import static com.VegaSolutions.lpptransit.utility.ScreenState.ERROR;
+import static com.VegaSolutions.lpptransit.utility.ScreenState.LOADING;
 
 public class DepartureActivity extends AppCompatActivity {
 
@@ -53,84 +48,173 @@ public class DepartureActivity extends AppCompatActivity {
     public static final String ROUTE_GARAGE = "route_garage";
 
     // Activity parameters
-    private String station_code;
-    private String station_name;
-    private String route_number;
-    private String route_name;
-    private boolean route_garage;
-
-    // Activity UI elements
-    @BindView(R.id.departure_route_name) TextView routeName;
-    @BindView(R.id.route_station_number) TextView routeNumber;
-    @BindView(R.id.departure_station_name) TextView stationName;
-    @BindView(R.id.station_center) TextView stationCenter;
-    @BindView(R.id.include) View routeNumberCircle;
-    @BindView(R.id.departure_rv) RecyclerView rv;
-    @BindView(R.id.header) FrameLayout header;
-    @BindView(R.id.departure_no_departures_error) View depErr;
-    @BindView(R.id.progressBar) ProgressBar progressBar;
-    @BindView(R.id.back) View back;
-    @BindView(R.id.timetable_title_tv) TextView title;
+    private String stationCode;
+    private String stationName;
+    private String routeNumber;
+    private String routeName;
+    private boolean routeGarage;
 
     private Adapter adapter;
+    private ElevationAnimation elevationAnimation;
 
     // Private local variables
-    private int textColor;
     private int backGroundColor;
 
-    private ElevationAnimation elevationAnimation;
+    private TravanaApp app;
+    private NetworkConnectivityManager networkConnectivityManager;
+    private DateTime now;
+
+    // Activity UI elements
+    TextView routeNameText;
+    TextView routeNumberText;
+    TextView stationNameText;
+    TextView stationCenterText;
+    View routeNumberCircleView;
+    TextView titleText;
+    RecyclerView rv;
+    FrameLayout header;
+    LinearLayout llNoDepartures;
+    ProgressBar progressBar;
+    View back;
+    LinearLayout errorContainer;
+    TextView errorText;
+    ImageView errorImageView;
+    TextView tryAgainText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(ViewGroupUtils.isDarkTheme(this) ? R.style.DarkTheme : R.style.WhiteTheme);
         setContentView(R.layout.activity_departure);
-        ButterKnife.bind(this);
+        initElements();
 
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | (ViewGroupUtils.isDarkTheme(this) ? 0 : View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR));
-        } else {
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        window.setStatusBarColor(Color.TRANSPARENT);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root), (i, insets) -> {
-            ViewGroup.MarginLayoutParams backParams = (ViewGroup.MarginLayoutParams) back.getLayoutParams();
-            backParams.setMargins(0, insets.getSystemWindowInsetTop(), 0, 0);
-            return insets.consumeSystemWindowInsets();
-        });
-
-        // Get theme default colors
-        int[] attribute = new int[] { android.R.attr.textColor, R.attr.backgroundViewColor };
-        TypedArray array = obtainStyledAttributes(ViewGroupUtils.isDarkTheme(this) ? R.style.DarkTheme : R.style.WhiteTheme, attribute);
-        textColor = array.getColor(0, Color.BLACK);
-        backGroundColor = array.getColor(1, Color.WHITE);
-        array.recycle();
+        app = TravanaApp.getInstance();
+        networkConnectivityManager = app.getNetworkConnectivityManager();
 
         // Get activity parameters
-        station_code = getIntent().getStringExtra(STATION_CODE);
-        station_name = getIntent().getStringExtra(STATION_NAME);
-        route_number = getIntent().getStringExtra(ROUTE_NUMBER);
-        route_name = getIntent().getStringExtra(ROUTE_NAME);
-        route_garage = getIntent().getBooleanExtra(ROUTE_GARAGE, false);
+        stationCode = getIntent().getStringExtra(STATION_CODE);
+        stationName = getIntent().getStringExtra(STATION_NAME);
+        routeNumber = getIntent().getStringExtra(ROUTE_NUMBER);
+        routeName = getIntent().getStringExtra(ROUTE_NAME);
+        routeGarage = getIntent().getBooleanExtra(ROUTE_GARAGE, false);
 
         // Set UI elements
-        if (route_garage)
-            route_name += " (" + getString(R.string.garage).toUpperCase() + ")";
-        routeName.setText(route_name );
-        routeNumber.setText(route_number);
-        routeNumber.setTextSize(16f);
-        stationName.setText(station_name);
-        stationCenter.setVisibility(Integer.parseInt(station_code) % 2 != 0 ? View.VISIBLE : View.GONE);
-        routeNumberCircle.getBackground().setTint(Colors.getColorFromString(route_number));
+        if (routeGarage)
+            routeName += " (" + getString(R.string.garage).toUpperCase() + ")";
+        routeNameText.setText(routeName);
+        routeNumberText.setText(routeNumber);
+        stationNameText.setText(stationName);
+        stationCenterText.setVisibility(Integer.parseInt(stationCode) % 2 != 0 ? View.VISIBLE : View.GONE);
+        routeNumberCircleView.getBackground().setTint(Colors.getColorFromString(routeNumber));
+
+        now = DateTime.now();
+        SimpleDateFormat sdf = new SimpleDateFormat("d. M", Locale.getDefault());
+        titleText.setText(getString(R.string.departures, sdf.format(now.toDate())));
+
+        retrieveDepartures();
+    }
+
+    private void retrieveDepartures() {
+
+        if (!networkConnectivityManager.isConnectionAvailable()) {
+            setupUi(ERROR);
+            setErrorUi(this.getResources().getString(R.string.no_internet_connection), R.drawable.ic_wifi);
+            return;
+        }
+        setupUi(LOADING);
+
+        // Get group number
+        String group = routeNumber.replaceAll("[^0-9]", "");
+
+        // Calculate for request
+        int next = 25 - now.getHourOfDay();
+        int prev = now.getHourOfDay();
+
+        Api.timetable(stationCode, next, prev, (apiResponse, statusCode, success) -> {
+            if (success) {
+                setupUi(DONE);
+
+                TimetableWrapper timetableWrapper = apiResponse.getData();
+                // Search for the right timetable
+                for (TimetableWrapper.RouteGroup.Route route : timetableWrapper.getRoute_groups().get(0).getRoutes()) {
+                    if (route.getParent_name().equals(routeName)) {
+                        runOnUiThread(() -> {
+                            adapter.setTimetables(route.getTimetable());
+                            // Notify user if empty
+                            if (adapter.timetables.isEmpty()) {
+                                llNoDepartures.setVisibility(View.VISIBLE);
+                            }
+                        });
+                        return;
+                    }
+                }
+            }
+
+            // In case of error, show error message
+            else {
+                setupUi(ERROR);
+                setErrorUi(this.getResources().getString(R.string.error_loading), R.drawable.ic_error_outline);
+            }
+        }, Integer.parseInt(group));
+    }
+
+    void setErrorUi(String errorName, int errorIconCode) {
+        runOnUiThread(() -> {
+            errorText.setText(errorName);
+            errorImageView.setImageResource(errorIconCode);
+        });
+    }
+
+    void setupUi(ScreenState screenState) {
+        runOnUiThread(() -> {
+            switch (screenState) {
+                case DONE: {
+                    this.progressBar.setVisibility(View.GONE);
+                    this.rv.setVisibility(View.VISIBLE);
+                    this.errorContainer.setVisibility(View.GONE);
+                    break;
+                }
+                case LOADING: {
+                    this.progressBar.setVisibility(View.VISIBLE);
+                    this.rv.setVisibility(View.GONE);
+                    this.errorContainer.setVisibility(View.GONE);
+                    break;
+                }
+                case ERROR: {
+                    this.progressBar.setVisibility(View.GONE);
+                    this.rv.setVisibility(View.GONE);
+                    this.errorContainer.setVisibility(View.VISIBLE);
+                    break;
+                }
+            }
+        });
+    }
+
+    void initElements() {
+        routeNameText = findViewById(R.id.departure_route_name);
+        routeNumberText = findViewById(R.id.route_station_number);
+        stationNameText = findViewById(R.id.departure_station_name);
+        stationCenterText = findViewById(R.id.station_center);
+        routeNumberCircleView = findViewById(R.id.route_station_circle);
+        titleText = findViewById(R.id.timetable_title_tv);
+        rv = findViewById(R.id.departure_rv);
+        header = findViewById(R.id.header);
+        llNoDepartures = findViewById(R.id.no_departures_container);
+        back = findViewById(R.id.back);
+        progressBar = findViewById(R.id.progress_bar);
+        errorText = findViewById(R.id.tv_error);
+        errorImageView = findViewById(R.id.iv_error);
+        tryAgainText = findViewById(R.id.tv_try_again);
+        errorContainer = findViewById(R.id.ll_error_container);
 
         back.setOnClickListener(v -> super.onBackPressed());
 
-        adapter = new Adapter();
+        tryAgainText.setOnClickListener(view -> {
+            retrieveDepartures();
+        });
 
+        adapter = new Adapter();
         elevationAnimation = new ElevationAnimation(16, header);
         rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(this));
@@ -142,51 +226,6 @@ public class DepartureActivity extends AppCompatActivity {
                 elevationAnimation.elevate(rv.canScrollVertically(-1));
             }
         });
-
-
-        // Get group number
-        String group = route_number.replaceAll("[^0-9]", "");
-
-        // Calculate for request
-        DateTime now = DateTime.now();
-        SimpleDateFormat sdf = new SimpleDateFormat("d. M", Locale.getDefault());
-        title.setText(getString(R.string.departures, sdf.format(now.toDate())));
-        int next = 25 - now.getHourOfDay();
-        int prev = now.getHourOfDay();
-
-        Api.timetable(station_code, next, prev, (apiResponse, statusCode, success) -> {
-            runOnUiThread(() -> {
-
-                // Remove progress bar
-                progressBar.setVisibility(View.GONE);
-                if (success) {
-                    TimetableWrapper timetableWrapper = apiResponse.getData();
-
-                    // Notify user if timetable is empty
-                    if (timetableWrapper.getRoute_groups().get(0).getRoutes().isEmpty())
-                        depErr.setVisibility(View.VISIBLE);
-
-                    // Search for the right timetable
-                    for (TimetableWrapper.RouteGroup.Route route : timetableWrapper.getRoute_groups().get(0).getRoutes()) {
-                        if (route.getParent_name().equals(route_name)) {
-                            adapter.setTimetables(route.getTimetable());
-                            // Notify user if empty
-                            if (adapter.timetables.isEmpty())
-                                depErr.setVisibility(View.VISIBLE);
-                            return;
-                        }
-                    }
-
-                    // If the right timetable was not found
-                    depErr.setVisibility(View.VISIBLE);
-                }
-
-                // In case of error, show Toast with error message
-                else new CustomToast(this).showDefault(statusCode);
-
-            });
-        }, Integer.parseInt(group));
-
     }
 
     private class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
@@ -196,6 +235,12 @@ public class DepartureActivity extends AppCompatActivity {
         private void setTimetables(List<TimetableWrapper.RouteGroup.Route.Timetable> timetables) {
             this.timetables = timetables;
             notifyDataSetChanged();
+
+            if (timetables.size() == 0) {
+                llNoDepartures.setVisibility(View.VISIBLE);
+            } else {
+                llNoDepartures.setVisibility(View.GONE);
+            }
         }
 
         @NonNull
@@ -224,8 +269,7 @@ public class DepartureActivity extends AppCompatActivity {
             if (timetable.isCurrent()) {
                 holder.container.getBackground().setTint(ColorUtils.blendARGB(backGroundColor, ViewGroupUtils.isDarkTheme(getApplication()) ? Color.WHITE : Color.GRAY, 0.1f));
             } else {
-                holder.hour.setTextColor(textColor);
-                holder.container.getBackground().setTint(backGroundColor);
+                holder.container.setBackgroundColor(backGroundColor);
             }
 
         }
@@ -239,7 +283,7 @@ public class DepartureActivity extends AppCompatActivity {
 
             TextView hour;
             FlexboxLayout minutes;
-            CardView container;
+            RelativeLayout container;
 
             private ViewHolder(@NonNull View itemView) {
                 super(itemView);
