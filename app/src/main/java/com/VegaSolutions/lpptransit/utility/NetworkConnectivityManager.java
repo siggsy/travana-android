68ff16c6ed698e2ecdccbd30f68d5ee7499eb481
Domen Kralj <domenkralj.sahist@gmail.com>
2021-04-02 19:58:05 +0200

    NetworkConnectivityManager added

---
+++ app/src/main/java/com/VegaSolutions/lpptransit/utility/NetworkConnectivityManager.java
@@ -0,0 +1,54 @@
+package com.VegaSolutions.lpptransit.utility;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+public class NetworkConnectivityManager {
+
+    private final Context context;
+    private boolean isConnectionAvailable = false;
+    private final ArrayList<ConnectivityManager.NetworkCallback> callbacks = new ArrayList<>();
+
+    public NetworkConnectivityManager(Context context) {
+        this.context = context;
+
+        ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                isConnectionAvailable = true;
+                for (ConnectivityManager.NetworkCallback callback : callbacks) {
+                    callback.onAvailable(network);
+                }
+            }
+
+            @Override
+            public void onLost(@NonNull Network network) {
+                isConnectionAvailable = false;
+                for (ConnectivityManager.NetworkCallback callback : callbacks) {
+                    callback.onLost(network);
+                }
+            }
+        };
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(networkCallback);
+        } else {
+            NetworkRequest request = new NetworkRequest.Builder().addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build();
+            connectivityManager.registerNetworkCallback(request, networkCallback);
+        }
+    }
+
+    public boolean isConnectionAvailable() {
+        return isConnectionAvailable;
+    }
+}
